0a3bfd52451dcdec5d9de7d0fad81ff1d9a9fe8d
Marcin <machikur2@gmail.com>
2020-11-10 18:01:45 +0000

    project starts

---
+++ src/test/java/com/example/CreditProject/CreditProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CreditProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
